800f518141a8fafaaf0d9f3e143c078bad8d075f
AnaChamba <40444447+AnaChamba@users.noreply.github.com>
2018-08-06 16:57:23 -0500

    Test
    SI TENGO N CONEJOS Y CADA CONEJO ES CAPAZ DE TENER 2 HIJOS CADA MES EN UN AÑO. ¿CUÁNTOS CONEJOS TENGO?

---
+++ Mi primera carpeta/script.js
@@ -0,0 +1,7 @@
+
+     
+    function potencia(){
+        let a = document.getElementById("txt1").value;
+        let b = document.getElementById("txt2").value;
+		document.getElementById("demo").innerHTML = Math.pow(a,b);
+	}
